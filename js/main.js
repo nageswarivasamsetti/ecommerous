ad091c01f45484240301aeef8c6b5523c7e5da46
nageswarivasamsetti <78140767+nageswarivasamsetti@users.noreply.github.com>
2021-02-01 16:06:01 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch("../data/data.json").then(response=>{
+fetch("https://nageswarivasamsetti.github.io/ecommerous/data/data.json").then(response=>{
 	return response.json()
 }).then(data=>{
      displayData(data)
